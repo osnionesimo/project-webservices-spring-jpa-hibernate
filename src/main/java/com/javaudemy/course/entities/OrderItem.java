83f7747f4f9cb0db9840ab478849de7ed40e49bd
Osni Onésimo <osnijunior841@gmail.com>
2024-07-10 11:53:13 -0300

    Subtotal & Total methods

--- src/main/java/com/javaudemy/course/entities/OrderItem.java
+++ src/main/java/com/javaudemy/course/entities/OrderItem.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_order_item")
@@ -61,5 +62,22 @@ public class OrderItem implements Serializable {
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    public Double getSubTotal() {
+        return price * quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
