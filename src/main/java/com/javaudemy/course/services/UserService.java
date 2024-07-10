b9e319bf29fe50a96102f52c438b4e609f9f9859
Osni Onésimo <osnijunior841@gmail.com>
2024-07-10 16:20:25 -0300

    Exception handling - update

--- src/main/java/com/javaudemy/course/services/UserService.java
+++ src/main/java/com/javaudemy/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.javaudemy.course.entities.User;
 import com.javaudemy.course.repositories.UserRepository;
 import com.javaudemy.course.services.exceptions.DatabaseException;
 import com.javaudemy.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(e.getMessage());
+        }
     }
 
     private void updateData(User entity, User obj) {
