5b22cf8fe89022ca745c9e0d891f3b114a0b78ec
Osni Onésimo <osnijunior841@gmail.com>
2024-07-10 14:56:46 -0300

    User update

--- src/main/java/com/javaudemy/course/resources/UserResource.java
+++ src/main/java/com/javaudemy/course/resources/UserResource.java
@@ -42,4 +42,10 @@ public class UserResource {
          service.deleteById(id);
          return ResponseEntity.noContent().build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
 }
